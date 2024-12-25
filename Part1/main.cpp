77fa27af3460bbdf52e50bab3f790aa1931a4713
Spencer Williams <swilliams94@go.pasadena.edu>
2024-12-25 15:56:54 -0800

    actually finished part 1

--- Part1/main.cpp
+++ Part1/main.cpp
@@ -4,13 +4,15 @@
 #include <filesystem>
 #include <vector>
 #include <array>
+#include <sstream>
 
 bool isLevelValid(std::string level);
+bool isNotInRange(int num1, int num2);
 
 int main() {
     std::ifstream infile;
 
-    infile.open("../testinput.txt");
+    infile.open("../input.txt");
     if (!infile.is_open()) {
         std::cout << "Input file opening failed." << std::endl;
     }
@@ -25,7 +27,10 @@ int main() {
     int numSafeReports = 0;
     for (std::string level : fileData) {
         if (isLevelValid(level)) {
+            std::cout << "Safe" << std::endl;
             numSafeReports++;
+        } else {
+            std::cout << "Unsafe" << std::endl;
         }
     }
 
@@ -34,21 +39,51 @@ int main() {
 }
 
 bool isLevelValid(std::string level) {
-    std::vector<char> levelChars = { level.c_str(), level.c_str() + level.length() };
-    
+
+    std::stringstream ss(level);
+    std::vector<int> levelNums;
+    std::string currentNum;
+
+    while (std::getline(ss, currentNum, ' ')) {
+        levelNums.push_back(stoi(currentNum));
+    }
+
+    ////std::vector<int> levelNums = { level.c_str(), level.c_str() + level.length() };
+
+    bool ascendingOrDescending = false;
     //*Check if the level is in ascending order
-    if (!std::is_sorted(levelChars.begin(), levelChars.end())) {
-        return false;
+    if (std::is_sorted(levelNums.begin(), levelNums.end())) {
+        ascendingOrDescending = true;
     }
 
     //*Check if the level is in descending order
-    std::reverse(levelChars.begin(), levelChars.end());
-    if (!std::is_sorted(levelChars.begin(), levelChars.end())) {
+    std::reverse(levelNums.begin(), levelNums.end());
+    if (std::is_sorted(levelNums.begin(), levelNums.end())) {
+        ascendingOrDescending = true;
+    }
+    std::reverse(levelNums.begin(), levelNums.end()); //Reverse back to normal
+
+    if (ascendingOrDescending == false) {
         return false;
     }
-    std::reverse(levelChars.begin(), levelChars.end()); //Reverse back to normal
 
-    //*Check 
+    //*Check that no two values are the same
+    if (std::adjacent_find(levelNums.begin(), levelNums.end()) != levelNums.end()) {
+        return false;
+    }
 
+    //*Check that adjacent values differ by 1-3
+    if (std::adjacent_find(levelNums.begin(), levelNums.end(), isNotInRange) != levelNums.end()) {
+        return false;
+    }
+
+    //* If all tests pass, the level is safe
     return true;
+}
+
+bool isNotInRange(int num1, int num2) {
+    if ((abs(num1 - num2) < 1) || (abs(num1 - num2) > 3)) {
+        return true;
+    }
+    return false;
 }
